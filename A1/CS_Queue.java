544dd6a47bc03565b3143dff0a285d7501c5f553
Narwhalx3 <36626217+Narwhalx3@users.noreply.github.com>
2018-03-05 13:32:02 +0100

    Update CS_Queue.java

--- A1/CS_Queue.java
+++ A1/CS_Queue.java
@@ -2,38 +2,43 @@ package concurrent_assignment2.A1;
 
 import concurrent_assignment2.A_intro.Queue;
 
-/**Use condition synchronization by means of busy wait.
- * 
- * What kind of variable do you need to implement busy
- * wait synchronization?
- * 
+/**
+ * Use condition synchronization by means of busy wait.
+ *
+ * What kind of variable do you need to implement busy wait synchronization?
+ *
  * Set a meaningful name for such variable.
- * */
- 
-class CS_Queue implements Queue{
-	int n=0;
-	
-	@Override
-	public void read() {
-		// TODO Auto-generated method stub
-		
-	}
+ *
+ */
+class CS_Queue implements Queue {
 
-	@Override
-	public void write(int x) {
-		// TODO Auto-generated method stub
-		
-	}
+    int n = 0;
+    volatile boolean reader_working = false;
 
-	@Override
-	public void read(int ID) {
-		// no need to implement this
-		
-	}
-	
-	
-	
-	
+    @Override
+    public void read() {
+        while (!reader_working) {
+        }
+        System.out.println(n);
+        reader_working = false;
+
+    }
+
+    @Override
+    public void write(int x) {
+        while (reader_working) {
+        }
+        n = x;
+        System.out.println(n);
+        reader_working = true;
+
+    }
+
+    @Override
+    public void read(int ID) {
+        // no need to implement this
+
+    }
 }
 
 
